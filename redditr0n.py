ac5a42b4f5717bf28c04916cc2cb00d9d4fd1997
oenghoff <oskar.enghoff@gmail.com>
2018-03-13 15:49:21 +0000

    Basic placeholder code

---
+++ redditr0n.py
@@ -0,0 +1,8 @@
+import sys
+import praw
+
+# reddit = praw.Reddit(client_id='x',
+#                      client_secret='y',
+#                      user_agent='z')
+
+print 'Argument list:', str(sys.argv)
